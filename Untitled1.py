f40f7e0831ca5cfd5cddf19cdb038bf0231e0fd0
Mrinmoy1998 <46236130+Mrinmoy1998@users.noreply.github.com>
2018-12-29 11:19:28 +0530

    Q.no:20

---
+++ Untitled1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#q.no:20
+
+string=input("enter string:")
+string=string.replace('a','*')
+print("modified string:")
+print(string)
+
+
+# In[ ]:
+
+
+
+
