563cf78c710c56f5b4df36ff1f5212ad0b28d3ed
Mrinmoy1998 <46236130+Mrinmoy1998@users.noreply.github.com>
2018-12-29 15:01:35 +0530

    Q.no:25

---
+++ Untitled9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#Q.no:25
+
+ch = input("Enter a character: ")
+if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+    print(ch, "is an Alphabet")
+else:
+    print(ch, "is not an Alphabet")
+if((ch>='1' and ch<='n') or (ch>='-1' and ch<='-n')):
+    print(ch, "is a Number")
+else:
+    print(ch, "is not a Number")
+
+
+# In[ ]:
+
+
+
+
