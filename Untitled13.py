6e71ef71fe1d522d7330ad382a654b22de185b88
Mrinmoy1998 <46236130+Mrinmoy1998@users.noreply.github.com>
2018-12-29 15:44:55 +0530

    Q.no:02

---
+++ Untitled13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Q.no:02
+
+str=input("enter the string:")
+char=input("enter the character whose number of occurences is to be found in the string:")
+c=str.count(char)
+print("number of occurences of occurences of {} is {}".format(char,c))
+
+
+# In[ ]:
+
+
+
+
