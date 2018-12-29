9a5a915a940f2c913edc98f2bd8ebe13cac9409b
Mrinmoy1998 <46236130+Mrinmoy1998@users.noreply.github.com>
2018-12-29 12:56:02 +0530

    Q.no:24

---
+++ Untitled6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Q.no:24
+
+num = int(input("Enter the number: "))
+
+print("Multiplication Table of", num)
+for i in range(1, 11):
+   print(num,"X",i,"=",num * i)
+
+
+# In[ ]:
+
+
+
+
